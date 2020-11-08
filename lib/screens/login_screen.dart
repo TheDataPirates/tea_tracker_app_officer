e27db58e1ff1c1a8b00e6b8b110ef5e01f58d39d
deveen <deveenrath@gmail.com>
2020-11-08 10:15:30 +0530

    merging with damitha5

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -24,6 +24,7 @@ class _LoginScreenState extends State<LoginScreen> {
       });
       final provider = Provider.of<Auth>(context, listen: false);
       try {
+        await provider.logout();
         await provider.login(_editedUser.user_id, _editedUser.password);
 
         Navigator.of(context).pushReplacementNamed('MainMenu');
@@ -64,104 +65,120 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     final mediaQuery = MediaQuery.of(context).size;
-    return Scaffold(
-      body: _isLoading
-          ? Center(
-              child: CircularProgressIndicator(),
-            )
-          : FormBuilder(
-              key: _fbkey,
-              child: Center(
-                child: Container(
-                  color: Colors.white,
-                  child: Padding(
-                    padding: const EdgeInsets.all(30.0),
-                    child: SingleChildScrollView(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          SizedBox(
-                            height: mediaQuery.height * 0.3,
-                            child: Image.asset(
-                              "images/logo.jpg",
-                              fit: BoxFit.contain,
-                            ),
-                          ),
-                          SizedBox(height: mediaQuery.height * 0.15),
-                          Container(
-                            child: FormBuilderTextField(
-                              attribute: "User-name",
-                              obscureText: false,
-                              style: style,
-                              decoration: InputDecoration(
-                                contentPadding:
-                                    EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-                                hintText: "User-name",
-                                border: OutlineInputBorder(
-                                    borderRadius: BorderRadius.circular(32.0)),
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage("images/bg1.jpg"),
+          fit: BoxFit.cover,
+          colorFilter: new ColorFilter.mode(
+              Colors.black.withOpacity(0.8), BlendMode.dstATop),
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: _isLoading
+            ? Center(
+                child: CircularProgressIndicator(),
+              )
+            : FormBuilder(
+                key: _fbkey,
+                child: Center(
+                  child: Container(
+//                    color: Colors.white,
+                    child: Padding(
+                      padding: const EdgeInsets.all(30.0),
+                      child: SingleChildScrollView(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            SizedBox(
+                              height: mediaQuery.height * 0.3,
+                              child: Image.asset(
+                                "images/logo.jpg",
+                                fit: BoxFit.contain,
                               ),
-                              validators: [FormBuilderValidators.required()],
-                              onSaved: (value) {
-                                _editedUser = User(
-                                    user_id: value,
-                                    password: _editedUser.password);
-                              },
                             ),
-                            height: mediaQuery.height * 0.1,
-                          ),
-                          SizedBox(height: mediaQuery.height * 0.01),
-                          Container(
-                            child: FormBuilderTextField(
-                              attribute: "Password",
-                              obscureText: true,
-                              style: style,
-                              decoration: InputDecoration(
-                                contentPadding:
-                                    EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-                                hintText: "Password",
-                                border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(32.0),
+                            SizedBox(height: mediaQuery.height * 0.15),
+                            Container(
+                              child: FormBuilderTextField(
+                                attribute: "User-name",
+                                obscureText: false,
+                                style: style,
+                                decoration: InputDecoration(
+                                  filled: true,
+                                  fillColor: Colors.white.withOpacity(0.9),
+                                  contentPadding: EdgeInsets.fromLTRB(
+                                      20.0, 15.0, 20.0, 15.0),
+                                  hintText: "User-name",
+                                  border: OutlineInputBorder(
+                                      borderRadius:
+                                          BorderRadius.circular(32.0)),
                                 ),
+                                validators: [FormBuilderValidators.required()],
+                                onSaved: (value) {
+                                  _editedUser = User(
+                                      user_id: value,
+                                      password: _editedUser.password);
+                                },
                               ),
-                              validators: [FormBuilderValidators.required()],
-                              onSaved: (value) {
-                                _editedUser = User(
-                                    user_id: _editedUser.user_id,
-                                    password: value);
-                              },
+                              height: mediaQuery.height * 0.1,
                             ),
-                            height: mediaQuery.height * 0.1,
-                          ),
-                          SizedBox(
-                            height: mediaQuery.height * 0.05,
-                          ),
-                          Material(
-                            elevation: 5.0,
-                            borderRadius: BorderRadius.circular(30.0),
-                            color: Theme.of(context).primaryColor,
-                            child: MaterialButton(
-                              minWidth: mediaQuery.width,
-                              padding:
-                                  EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-                              onPressed: submit,
-                              child: Text("Login",
-                                  textAlign: TextAlign.center,
-                                  style: style.copyWith(
-                                      color: Colors.white,
-                                      fontWeight: FontWeight.bold)),
+                            SizedBox(height: mediaQuery.height * 0.01),
+                            Container(
+                              child: FormBuilderTextField(
+                                attribute: "Password",
+                                obscureText: true,
+                                style: style,
+                                decoration: InputDecoration(
+                                  filled: true,
+                                  fillColor: Colors.white.withOpacity(0.9),
+                                  contentPadding: EdgeInsets.fromLTRB(
+                                      20.0, 15.0, 20.0, 15.0),
+                                  hintText: "Password",
+                                  border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(32.0),
+                                  ),
+                                ),
+                                validators: [FormBuilderValidators.required()],
+                                onSaved: (value) {
+                                  _editedUser = User(
+                                      user_id: _editedUser.user_id,
+                                      password: value);
+                                },
+                              ),
+                              height: mediaQuery.height * 0.1,
                             ),
-                          ),
-                          SizedBox(
-                            height: mediaQuery.height * 0.1,
-                          ),
-                        ],
+                            SizedBox(
+                              height: mediaQuery.height * 0.05,
+                            ),
+                            Material(
+                              elevation: 5.0,
+                              borderRadius: BorderRadius.circular(30.0),
+                              color: Theme.of(context).primaryColor,
+                              child: MaterialButton(
+                                minWidth: mediaQuery.width,
+                                padding:
+                                    EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+                                onPressed: submit,
+                                child: Text("Login",
+                                    textAlign: TextAlign.center,
+                                    style: style.copyWith(
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold)),
+                              ),
+                            ),
+                            SizedBox(
+                              height: mediaQuery.height * 0.1,
+                            ),
+                          ],
+                        ),
                       ),
                     ),
                   ),
                 ),
               ),
-            ),
+      ),
     );
   }
 }
